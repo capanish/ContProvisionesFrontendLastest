d7816c1b88c820181a63375a23b971f118f671be
pragathibaskar <47776430+pragathibaskar@users.noreply.github.com>
2019-10-18 14:18:49 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ import { Router, NavigationEnd } from '@angular/router';
 })
 export class AppComponent implements OnInit {
   title = 'adif';
-  toggle = false;
   showMenu = false;
   constructor(
     private router: Router
@@ -25,13 +24,5 @@ export class AppComponent implements OnInit {
 
   show() {
     this.showMenu = !this.showMenu;
-  }
-  @HostListener('body:click', ['$event'])
-  onclick(event) {
-    const ele: HTMLElement = <HTMLElement> event.target;
-    const eleClass = ele.parentElement.parentElement;
-    if (eleClass && !eleClass.classList.contains('matMenu')) {
-      this.toggle = false;
-    }
   }
 }
